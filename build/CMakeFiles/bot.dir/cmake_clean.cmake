db18217ff71fe6caf0e4f0757f30d4937910939a
Sonkeng Maldini <sdmg15@pm.me>
2021-08-23 22:36:55 +0100

    Initial Commit

---
+++ build/CMakeFiles/bot.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bot.dir/src/main.cpp.o"
+  "CMakeFiles/bot.dir/src/main.cpp.o.d"
+  "bot"
+  "bot.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/bot.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
