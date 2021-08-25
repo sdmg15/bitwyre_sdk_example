eed9ed0df0f77feb9c841dbe7b580db4840a4dd5
Sonkeng Maldini <sdmg15@pm.me>
2021-08-25 09:49:38 +0100

    feat: testing market endpoint

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
-#include "bitwyresdk/rest/public/Time.hpp"
+#include "rest/public/Market.hpp"
 
+using namespace Bitwyre::Rest;
 auto main() -> int {
+    auto r = Public::Market::get();
 
+    std::cout << r.markets.size() << "\n";
+
+    for(const auto& m : r.markets) {
+        std::cout << m << "\n";
+    }
 }
